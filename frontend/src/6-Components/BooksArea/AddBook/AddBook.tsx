a28bf38e9316085509f150718db325e86bfdf86e
Michal Shenkar <0583294585m@gmail.com>
2023-05-09 22:37:41 +0300

    frontend - add form fir adding book

--- frontend/src/6-Components/BooksArea/AddBook/AddBook.tsx
+++ frontend/src/6-Components/BooksArea/AddBook/AddBook.tsx
@@ -1,9 +1,31 @@
+import { useForm } from "react-hook-form"
 import "./AddBook.css"
+import BooksModel from "../../../3-Models/BooksModel"
 
 function AddBook(): JSX.Element{
+
+    const {register, handleSubmit} = useForm<BooksModel>()
+    function addBook(newBook: BooksModel){
+
+    }
     return(
         <div className="AddBook">
+            <form onSubmit={handleSubmit(addBook)}>
+                <label>Name:</label>
+                <input type="text" {...register("bookName")} required/> <br />
 
+                <label>Summary:</label>
+                <textarea {...register("bookSummary")} required></textarea>  <br />
+
+                <label>Price:</label>
+                <input type="text" {...register("bookPrice")} required/> <br />
+
+                <label>Stock:</label>
+                <input type="text" {...register("bookStock")} required/> <br />
+
+                <button>Add Book</button>
+
+            </form>
         </div>
     )
 }
