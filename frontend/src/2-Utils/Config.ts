6a9f021124a76f3eea649790078d6a761d294b96
Michal Shenkar <0583294585m@gmail.com>
2023-05-09 23:42:26 +0300

    frontend - add auth service

--- frontend/src/2-Utils/Config.ts
+++ frontend/src/2-Utils/Config.ts
@@ -1,6 +1,8 @@
 class Config {
     public booksTypesURL = "http://localhost:3001/api/books-types/"
     public booksURL = "http://localhost:3001/api/books/"
+    public registerURL = "http://localhost:3001/api/auth/register/"
+    public loginURL = "http://localhost:3001/api/auth/login/"
 }
 
 const appConfig = new Config()
---
+++ frontend/src/4-Service/Auth-Service.ts
@@ -0,0 +1,29 @@
+import axios from "axios"
+import CredentialsModel from "../3-Models/CredentialsModel"
+import UserModel from "../3-Models/UserModel"
+import appConfig from "../2-Utils/Config"
+
+class AuthService {
+
+
+    public async register(user:UserModel): Promise<void> {
+        const response = await axios.post<string>(appConfig.registerURL, user)
+        const token = response.data
+        sessionStorage.setItem("token", token)  
+    }
+
+    public async login(credential:CredentialsModel): Promise<void> {
+        const response = await axios.post<string>(appConfig.loginURL, credential)
+        const token = response.data
+        sessionStorage.setItem("token", token)  
+    }
+
+    public logout() {
+        sessionStorage.removeItem("token")
+    }
+}
+
+
+
+const authService = new AuthService()
+export default authService
