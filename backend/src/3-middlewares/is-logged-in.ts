43000db67a90a7291d266193ac236399e53ae17f
Michal Shenkar <0583294585m@gmail.com>
2023-05-10 00:21:02 +0300

    backend - auth - add middleware of isLogged

--- backend/src/3-middlewares/is-logged-in.ts
+++ backend/src/3-middlewares/is-logged-in.ts
@@ -1,10 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
+import cyber from '../2-utils/cyber'
+import { UnAuthorizedErrorModel } from '../4-models/errors-model'
 
 
-function isLoggedIn(request: Request, response: Response, next: NextFunction) {
+async function isLoggedIn(request: Request, response: Response, next: NextFunction) {
 
-    const err = new RouteNotFoundErrorModel(request.originalUrl)
-    next(err)
+    const isLogged = await cyber.verifyToken(request)
+    if(!isLogged) throw new UnAuthorizedErrorModel("Invalid token")
+
+    next()
 }
 
 export default isLoggedIn
