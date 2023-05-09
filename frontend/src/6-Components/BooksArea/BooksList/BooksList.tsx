4ba9212044d5147d493fe0e3a986b50e65278011
Michal Shenkar <0583294585m@gmail.com>
2023-05-09 22:19:36 +0300

    frontend - add books-list

--- frontend/src/6-Components/BooksArea/BooksList/BooksList.tsx
+++ frontend/src/6-Components/BooksArea/BooksList/BooksList.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
 import "./BooksList.css"
+import BooksModel from "../../../3-Models/BooksModel"
+import booksService from "../../../4-Service/BooksService"
+import BookCard from "../BookCard/BookCard"
 
 function BooksList(): JSX.Element{
+
+    const [books, setBooks] = useState<BooksModel[]>([])
+
+    useEffect(()=>{
+        booksService.getAllBooks()
+            .then(b=>setBooks(b))
+            .catch(err => alert(err))
+    },[])
+
     return(
         <div className="BooksList">
-
+            {books.map(b => <BookCard key={b.bookId} book={b} />)}
         </div>
     )
 }
